4ba49f68af97b85cd7e9c138db81c1924e356812
Ваше Имя <panda@sultonoway.uz>
2025-08-31 18:51:55 +0500

    commit

--- src/app/lib/mongo.ts
+++ src/app/lib/mongo.ts
@@ -1,23 +1,19 @@
 import { MongoClient, Db } from "mongodb";
 
-// Extend the global type to include our MongoDB client promise
 declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 const uri = process.env.MONGO_URI!;
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-let db: Db;
-
 if (!uri) throw new Error("⚠️ Укажи MONGO_URI в .env.local");
 
-// Используем глобальный объект, чтобы избежать повторных подключений на hot reload
-if (!globalThis._mongoClientPromise) {
-  client = new MongoClient(uri);
-  globalThis._mongoClientPromise = client.connect();
-}
-clientPromise = globalThis._mongoClientPromise;
+let db: Db;
+
+const client = new MongoClient(uri);
+
+const clientPromise =
+  globalThis._mongoClientPromise ??
+  (globalThis._mongoClientPromise = client.connect());
 
 export const getDB = async () => {
   if (!db) {
